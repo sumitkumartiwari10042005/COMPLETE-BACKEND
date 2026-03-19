19eb2a7050242f287eb4697bf21411ae4191b326
kumar <kumarsumit10042005@gmail.com>
2026-03-19 17:23:52 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,49 @@
+const express=require('express')
+
+const app=express();
+
+app.use(express.json());
+
+const notes=[];
+
+app.post("/notes",(req,res)=>{
+    notes.push(req.body)
+
+    res.status(201).json({
+        message:"note created successfully"
+    })
+})
+
+app.get("/notes",(req,res)=>{
+    res.status(200).json({
+        message:"notes fetched successfully",
+        notes:notes
+    })
+})
+
+app.delete('/notes/:index',(req,res)=>{
+
+    const index=req.params.index;
+  
+    delete notes[index] 
+    
+    res.status(200).json({
+        message:"note deleted successfully"
+    })
+})
+
+
+app.patch("/notes/:index",(req,res)=>{
+
+    const index=req.params.index;
+
+    const description= req.body.description
+
+    notes[index].description=description;
+
+    res.status(200).json({
+        message:"note updated successfully"
+    })
+})
+
+module.exports=app;
