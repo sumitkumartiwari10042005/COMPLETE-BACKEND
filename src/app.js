80864052c2d0cc962c34bf3ca0c36581b77ae9f5
kumar <kumarsumit10042005@gmail.com>
2026-03-19 17:29:34 +0530

    changed post method of notes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.post("/notes",(req,res)=>{
     notes.push(req.body)
 
     res.status(201).json({
-        message:"note created successfully"
+        message:"note created successfullyy"
     })
 })
 
